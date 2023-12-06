1ff57f548ffc7a95b09f5e6ee0b4b674c888c319
mentekid <mentekid@gmail.com>
2023-12-05 20:39:39 -0500

    Add solution to part1 of day2 2023

---
+++ src/aoc2023/cube_conundrum.py
@@ -0,0 +1,35 @@
+import re
+import sys
+
+MAX_RED = 12
+MAX_GREEN = 13
+MAX_BLUE = 14
+
+def get_counts(string, color):
+    matches = re.findall(f"(\d+) {color}", string)
+    if matches:
+        return [int(m) for m in matches]
+
+def parse_line(line):
+    game, results = line.split(":")
+
+    reds = get_counts(results, "red")
+    greens = get_counts(results, "green")
+    blues = get_counts(results, "blue")
+
+    game_no = re.findall("Game (\d+)", game)
+
+    return reds, greens, blues, int(game_no[0])
+
+def is_possible(reds, greens, blues):
+    return max(reds) <= MAX_RED and max(greens) <= MAX_GREEN and max(blues) <= MAX_BLUE
+
+def solve(lines):
+    answer = 0
+    for line in lines:
+        reds, greens, blues, game_idx = parse_line(line)
+        answer += game_idx if is_possible(reds, greens, blues) else 0
+    return answer
+
+if __name__ == "__main__":
+    print(solve([line for line in sys.stdin]))
---
+++ test/aoc2023/test_cube_conundrum.py
@@ -0,0 +1,4 @@
+from src.aoc2023.cube_conundrum import solve
+
+def test_solve():
+    assert(solve(["Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"]) == 1)
